dacbd094fc0e734691fb131275d6ff807198624d
milesalex <howdy@alexmilesdesign.com>
2012-04-03 02:03:03 -0400

    First commit

---
+++ triangle.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a,b,c;
+    double d,e,f;
+    
+    cout << "Enter a: ";
+    cin >> a;
+    
+    cout << "Enter b: ";
+    cin >> b;
+    
+    d = pow(a, 2.0);
+    e = pow(b, 2.0);
+    f = d+e;
+    
+    c = pow(f, 1.0/2.0);
+    cout << "C is: " << c << endl;
+    
+    //return 0;
+    cin.get();
+}
+
